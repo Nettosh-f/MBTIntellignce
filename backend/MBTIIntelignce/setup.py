48e4b12ea7fbd64c4dcc1ce664130d9d1b639583
tbs <tom.bensinai@gmail.com>
2025-04-05 16:56:59 +0300

    wip: started working on frontend, adde docker and docker compose files

---
+++ backend/MBTIIntelignce/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MBTIIntelignce",
+    description="",
+    version="0.0.1",
+    author="",
+    packages=find_packages(where="."),
+)
